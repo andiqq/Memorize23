921b5f84b1673e4ea4ffc4ac6a9cf41f4c829f10
Dr. Andreas Plagens <andreas@plagens.eu>
2023-08-19 09:21:04 +0200

    Initial Commit

---
+++ Memorize23/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memorize23
+//
+//  Created by Dr. Andreas Plagens on 19.08.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize23/Memorize23App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize23App.swift
+//  Memorize23
+//
+//  Created by Dr. Andreas Plagens on 19.08.23.
+//
+
+import SwiftUI
+
+@main
+struct Memorize23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
