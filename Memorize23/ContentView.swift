ae0330168b3eaeb7a524ed496a303408b27607ec
Dr. Andreas Plagens <andreas@plagens.eu>
2023-08-21 21:40:27 +0200

    Togglable isFaceUp

--- Memorize23/ContentView.swift
+++ Memorize23/ContentView.swift
@@ -27,21 +27,20 @@ struct ContentView: View {
 
 struct CardView: View {
     
-    var isFaceUp: Bool = false
+    @State var isFaceUp: Bool = false
     
     var body: some View {
-        ZStack(content: {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("👻").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
-            
-        })
+        }
+        .onTapGesture { isFaceUp.toggle() }
     }
 }
 
