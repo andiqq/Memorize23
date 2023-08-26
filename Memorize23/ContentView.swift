68144e5260d12f6fddf58d444f19714678911678
Dr. Andreas Plagens <andreas@plagens.eu>
2023-08-26 11:26:44 +0200

    removed the + and - buttons

--- Memorize23/ContentView.swift
+++ Memorize23/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
                 cards
             }
             Spacer()
-            cardCountAdjusters
+            
         }
         .padding()
     }
@@ -38,33 +38,7 @@ struct ContentView: View {
         }
         .foregroundColor(.orange)
     }
-    
-    var cardCountAdjusters:some View {
-        HStack {
-            cardRemover
-            Spacer()
-            cardAdder
-        }
-        .imageScale(.large)
-        .font(.largeTitle)
-    }
-    
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
-        Button(action: {
-            cardCount += offset
-        }, label: {
-            Image(systemName: symbol)
-        })
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
-    }
-    
-    var cardRemover: some View {
-       cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
-    }
-    
-    var cardAdder: some View {
-        cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
-    }
+
 }
 
 struct CardView: View {
